db80cc6a3d497abec7b2a68ab3a340cc210f3095
Shootie <jose.f.rocha92@gmail.com>
2024-05-05 15:07:54 +0100

    fixing routes

---
+++ js/script.js
@@ -0,0 +1,42 @@
+/* Remove grid class from Skills */
+function removeGridContainer() {
+    var div = document.getElementById('skill');
+    if (window.innerWidth >= 200 && window.innerWidth <= 1024) {
+      div.classList.remove('grid-container');
+    } else {
+      div.classList.add('grid-container');
+    }
+}
+  
+
+window.onload = function() {
+    removeGridContainer();
+};
+
+window.addEventListener('resize', function() {
+    removeGridContainer();
+});
+
+/* Remove flex class from Technical Skills */
+function removeFlexTecSkill() {
+    var div = document.getElementById('teck-skill');
+    if (window.innerWidth >= 200 && window.innerWidth <= 1024) {
+      div.classList.remove('flex-container');
+    } else {
+      div.classList.add('flex-container');
+    }
+}
+  
+
+window.onload = function() {
+    removeFlexTecSkill();
+};
+
+window.addEventListener('resize', function() {
+    removeFlexTecSkill();
+});
+
+
+/* Call Functions */
+translateElements('en');
+setActiveButton('en');
